0941b9aba9d605059ea8e71676bc5050651e7361
eirian10 <nhujuank1009@email.com>
2026-05-16 17:59:00 +0700

    update cấu trúc database

--- QuanLyDiem/Models/Student.cs
+++ QuanLyDiem/Models/Student.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace QuanLyDiem.Models
 {
@@ -13,23 +14,29 @@ namespace QuanLyDiem.Models
         [RegularExpression(@"^\d{10}$", ErrorMessage = "Mã sinh viên phải bao gồm chính xác 10 chữ")]
         public string StudentCode { get; set; }
 
-        [Required(ErrorMessage = "Họ tên không được để trống")]
-        [MaxLength(100)]
-        public string FullName { get; set; }
+        [Required(ErrorMessage = "Họ và tên lót không được để trống.")]
+        public string LastName { get; set; } = null!;
+
+        [Required(ErrorMessage = "Tên sinh viên không được để trống.")]
+        public string FirstName { get; set; } = null!;
+
+        [Required(ErrorMessage = "Vui lòng chọn giới tính.")]
+        public string Gender { get; set; } = null!;
 
         [Required(ErrorMessage = "Ngày sinh là bắt buộc")]
         [DataType(DataType.Date)]
-        [Display(Name = "Ngày sinh")]
         public DateTime DateOfBirth { get; set; }
 
-        [Required(ErrorMessage = "Lớp sinh hoạt không được để trống")]
-        [MaxLength(50)]
-        public string HomeroomClass { get; set; } // Lớp sinh hoạt (VD: CNTT K44)
+        [Required(ErrorMessage = "Vui lòng chọn lớp sinh hoạt.")]
+        public int HomeroomClassId { get; set; }
+
+        [ForeignKey("HomeroomClassId")]
+        public HomeroomClass? HomeroomClass { get; set; }
 
         [EmailAddress(ErrorMessage = "Email không đúng định dạng")]
         [Required(ErrorMessage = "Email là bắt buộc")]
         public string Email { get; set; }
 
-        public ICollection<Enrollment>? Enrollments { get; set; } //update
+        public ICollection<Enrollment>? Enrollments { get; set; } = new List<Enrollment>();
     }
 }
