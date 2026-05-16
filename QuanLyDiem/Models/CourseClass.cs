d2569279a557545214d0394828c689fe9d85d7be
eirian10 <nhujuank1009@email.com>
2026-05-16 17:57:11 +0700

    update mã học kì

--- QuanLyDiem/Models/CourseClass.cs
+++ QuanLyDiem/Models/CourseClass.cs
@@ -12,20 +12,23 @@ namespace QuanLyDiem.Models
 
         [Required(ErrorMessage = "Mã lớp học phần không được để trống")]
         [MaxLength(50)]
-        public int ClassCode { get; set; } // VD: IT01_N01
+        public string ClassCode { get; set; } // VD: IT01_N01
 
-        [Required(ErrorMessage = "Học kỳ không được để trống")]
-        [MaxLength(20)]
-        public string Semester { get; set; } // VD: HK1_2025_2026
+        [Required(ErrorMessage = "Vui lòng chọn học kỳ.")]
+        public int SemesterId { get; set; }
 
+        [ForeignKey("SemesterId")]
+        public Semester? Semester { get; set; }
+
+        [Required(ErrorMessage = "Vui lòng chọn môn học.")]
         public int SubjectId { get; set; }
         [ForeignKey("SubjectId")]
-        public Subject Subject { get; set; }
+        public Subject? Subject { get; set; }
 
         public int? LecturerId { get; set; }
         [ForeignKey("LecturerId")]
-        public User Lecturer { get; set; }
+        public User? Lecturer { get; set; }
 
-        public ICollection<Enrollment> Enrollments { get; set; }
+        public ICollection<Enrollment>? Enrollments { get; set; } = new List<Enrollment>();
     }
 }
