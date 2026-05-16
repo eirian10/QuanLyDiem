c8621c9d176ae97c2d00d7747cf491f0ad942d37
eirian10 <nhujuank1009@email.com>
2026-05-16 17:55:18 +0700

    update database

--- QuanLyDiem/Migrations/20260516103428_InitialCreate_V3.Designer.cs
+++ QuanLyDiem/Migrations/20260516103428_InitialCreate_V3.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using QuanLyDiem.Data;
 
@@ -10,9 +11,11 @@ using QuanLyDiem.Data;
 namespace QuanLyDiem.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260516103428_InitialCreate_V3")]
+    partial class InitialCreate_V3
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.4");
